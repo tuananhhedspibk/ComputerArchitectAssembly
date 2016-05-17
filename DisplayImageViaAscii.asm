9829f0f68d374a0312147e472b50239c841378b7
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2016-05-17 16:09:40 +0700

    Display Image Via Ascii Character

--- DisplayImageViaAscii.asm
+++ DisplayImageViaAscii.asm
@@ -1,340 +1,266 @@
 .data
+	line00: 	.asciiz "                                                *************  \n "
+	line01: 	.asciiz "**************                                 *3333333333333* \n "
+	line02: 	.asciiz "*222222222222222*                              *33333********  \n "
+	line03: 	.asciiz "*22222******222222*                            *33333*         \n "
+	line04: 	.asciiz "*22222*      *22222*                           *33333********  \n "
+	line05: 	.asciiz "*22222*       *22222*        *************     *3333333333333* \n "
+	line06: 	.asciiz "*22222*       *22222*      **11111*****111*    *33333********  \n "
+	line07: 	.asciiz "*22222*       *22222*    **11111****           *33333*         \n "
+	line08: 	.asciiz "*22222*       *22222*    *11111*               *33333********  \n "
+	line09:	.asciiz "*22222*******222222*     *11111*               *3333333333333* \n "
+	line10:	.asciiz "*2222222222222222*       *11111*                *************  \n "
+	line11:	.asciiz "***************          *11111*                               \n "
+	line12:	.asciiz "   -----                 *11111*                               \n "
+	line13:	.asciiz "  /  o o \\               *11111*********                       \n "
+	line14:	.asciiz "  \\    > /                 **111111***111*                     \n "
+	line15:	.asciiz "   -----                      ***********     dce.hust.edu.vn  \n "
+	endofline: .asciiz "\n\n"
 	
-	a1: 	.asciiz "                                                          *************  \n "
-	a2: 	.asciiz "**************                                           *3333333333333* \n "
-	a3: 	.asciiz "*222222222222222*                                        *33333********  \n "
-	a4: 	.asciiz "*22222******222222*                                      *33333*         \n "
-   	a5: 	.asciiz "*22222*      *22222*                                     *33333********  \n "
-	a6: 	.asciiz "*22222*       *22222*        *************               *3333333333333* \n "
-	a7: 	.asciiz "*22222*       *22222*      **11111*****111*              *33333********  \n "
-	a8: 	.asciiz "*22222*       *22222*    **11111****                     *33333*         \n "
-	a9: 	.asciiz "*22222*       *22222*    *11111*                         *33333********  \n "
-	a10:	.asciiz "*22222*******222222*     *11111*                         *3333333333333* \n "
-	a11:	.asciiz "*2222222222222222*       *11111*                          *************  \n "
-	a12:	.asciiz "***************          *11111*                                         \n "
-	a13:	.asciiz "   -----                 *11111*                                         \n "
-	a14:	.asciiz "  /  o o \\               *11111*********                                 \n "
-	a15:	.asciiz "  \\    > /                 **111111***111*                               \n "
-	a16:	.asciiz "   -----                      ***********                dce.hust.edu.vn \n "
-				
+	ms1:	.asciiz " \n Input Color of D (0->9) : "
+	ms2:	.asciiz	" \n Input Color of C (0->9) : "
+	ms3:	.asciiz	" \n Input Color of E (0->9) : "
 	
-	ms1:	.asciiz	"\n\n                         Hinh anh ban dau \n"
-	ms2:	.asciiz "\n\n			Hinh anh sau khi xoa mau \n"
-	ms3:	.asciiz "\n\n			Hinh anh sau khi dao \n"
-	ms4:	.asciiz "\n\n		 Hinh anh sau khi doi mau \n"
-	
-	ms7:	.asciiz " \n Nhap mau cho chu D (0->9) : "
-	ms8:	.asciiz	" \n Nhap mau cho chu C (0->9) : "
-	ms9:	.asciiz	" \n Nhap mau cho chu E (0->9) : "
-	
-	ms10:	.asciiz	"\n  MENU \n"
-	ms11:	.asciiz " 1. In Anh \n"
-	ms12:	.asciiz " 2. Xoa Mau \n"
-	ms13:	.asciiz " 3. Dao Chu \n"
-	ms14:	.asciiz	" 4. Doi Mau \n"
-	ms15:	.asciiz " 5. Exit \n"
-	ms16:	.asciiz	" Hay nhap lua chon 1->5 : "	
+	menu1: .asciiz "\n1. Show Image."
+	menu2:	.asciiz "\n2. Delete color."
+	menu3:	.asciiz "\n3. Change Position."
+	menu4: .asciiz "\n4. Change Color."
+	menu5: .asciiz "\n5. Exit Program."
+	menu6:	.asciiz "\n>>>> Chose Function:"
+################################################################################
 .text
-#--------------------------------------------------------------------------------------------	
-#define
-	la	$s0, a1		# $s0 chua dia chi cua xau a1
-	la	$s1, a16	# $s1 chua dia chi cua xau a16
+	la	$s0, line00					# $s0 is address of line00
+	la	$s1, line15					# $s1 is address of line15
 	
-	
-	li	$s2, '2'	# ki tu mau chu D
-	li	$s3, '1'	# ki tu mau chu E
-	li	$s4, '3'	# ki tu mau chu C
-#---------------------------------------------------------------------------------------------		
-	
-#in MENU
-
-MENU:
-	li	$v0, 4
-	la	$a0, ms10
+	li	$s2, '2'					# s2 is colof of D
+	li	$s3, '1'					# s3 is color of E
+	li	$s4, '3'					# s4 if color of C
+################################################################################
+Menu:
+	li	$v0, 4						# Menu1 :
+	la	$a0, menu1
+	syscall
+	li	$v0, 4						# Menu2:
+	la	$a0, menu2
+	syscall
+	li	$v0, 4						# Menu3:
+	la	$a0, menu3
+	syscall
+	li	$v0, 4						# Menu4 :
+	la	$a0, menu4
+	syscall
+	li	$v0, 4						# Menu5:
+	la	$a0, menu5
+	syscall
+	li	$v0, 4						# Menu6:
+	la	$a0, menu6
 	syscall
 	
-	li	$v0, 4
-	la	$a0, ms11
+	li	$v0, 12					
 	syscall
-
-	li	$v0, 4
-	la	$a0, ms12
-	syscall
+	move	$t9, $v0					# t9 = v0 = function was chosen
 	
-	li	$v0, 4
-	la	$a0, ms13
-	syscall
+	beq	$t9, '1', Case1
+	beq	$t9, '2', Case2
+	beq	$t9, '3', Case3
+	beq	$t9, '4', Case4
+	beq	$t9, '5', EXIT	
 	
-	li	$v0, 4
-	la	$a0, ms14
-	syscall
+	j 	Menu
+################################################################################	
+Case1	:							# Show Image		
 	
-	li	$v0, 4
-	la	$a0, ms15
+	li 	$v0, 4						# print \n
+	la 	$a0, endofline
 	syscall
 	
-	li	$v0, 4
-	la	$a0, ms16
+	jal 	ShowImage				 
+	j 	Menu
+	
+	ShowImage:								
+		move 	$t0, $s0				# t0 is address of line00
+		Case1_Loop:				
+			li 	$v0, 4
+			move 	$a0, $t0
+			syscall
+			
+			addi 	$t0, $t0, 66			# t0 is next string, 66 is length of each string
+			ble 	$t0, $s1, Case1_Loop  		# until to last line (s1)
+	
+		jr 	$ra
+################################################################################
+Case2:								# Delete color
+	li 	$v0, 4						# print \n
+	la	$a0, endofline
 	syscall
 	
-	li	$v0, 12		# Nhap lua chon
+	move 	$t8, $s0					# t8 = s0 = first line
+	Case2_Loop:	
+		move 	$t0, $t8				# t0 = t8 = first character int line
+		li	$t7, 0					# count character
+	
+		CountCharacter:	
+		lb 	$t3, 0($t0)				# t3 save character in line
+	
+		beq	$t3, $s2, PrintSpace			# if character if color, print space
+		beq	$t3, $s3, PrintSpace
+		beq	$t3, $s4, PrintSpace
+		Print:  						# print character isn't color	
+			li	$v0, 11
+			move	$a0, $t3
+			syscall
+			j 	Case2_Continue
+		PrintSpace:					# print space
+			li 	$t4, ' '
+			li	$v0, 11
+			move	$a0, $t4 
+			syscall
+		Case2_Continue:	
+			addi 	$t7, $t7, 1			# 
+			addi	$t0, $t0, 1			# 
+			ble 	$t7, 65, CountCharacter		# traverse all character in line
+	
+		addi 	$t8, $t8, 66				# traverse whole lines
+		ble 	$t8, $s1, Case2_Loop
+	
+	j 	Menu
+################################################################################ 	
+Case3:								# Change position
+	li 	$v0, 4						# print \n
+	la	$a0, endofline
 	syscall
-	move	$t9, $v0	# t9 chua lua chon 
-	
-	beq	$t9, '5', EXIT
-	beq	$t9, '1', yc1
-	beq	$t9, '2', yc2
-	beq	$t9, '3', yc3
-	beq	$t9, '4', yc4	
-	
-	j MENU
-#-----------------------------------------------------------------------------------------------
-
-yc1:				# in anh			
-	
-	li 	$v0, 4		# in messsage1
-	la 	$a0, ms1
-	syscall
-	
-	jal 	print		# goi thu tuc in 
-	j MENU
-	
-print:				# thu tuc in 		
-	move 	$t0, $s0	# in xau a1->a16
-	
-loop:				
-	li 	$v0, 4
-	move 	$a0, $t0
-	syscall
-	
-	addi 	$t0, $t0, 76	# in xau tiep theo
-	ble 	$t0, $s1, loop	# in den xau cuoi thi dung	
-	
-	jr 	$ra
-#-----------------------------------------------------------------------------------------------------
-yc2:				# xoa mau
-
-	li 	$v0, 4		# in ra message2
-	la	$a0, ms2
-	syscall
-	
-	move 	$t8, $s0	# t8 chua dia chi xau dau tien
-	
-loop2:	
-	move 	$t0, $t8	
-	li	$t7, 0		# $t7 = i, i = 0
-	
-x:	lb 	$t3, 0($t0) 	# t3 chua ki tu trong xau
-	
-	
-	beq	$t3, $s2, inspace# kiem tra neu la ki tu mau thi in ra space
-	beq	$t3, $s3, inspace
-	beq	$t3, $s4, inspace
-	
-in:  				# in ki tu khong phai ki tu mau	
-	li	$v0,11
-	move	$a0, $t3
-	syscall
-	j next
-	
-inspace:			# in space
-	li 	$t4, ' '
-	li	$v0,11
-	move	$a0,$t4 
-	syscall
-next:	
-	addi 	$t7, $t7, 1	# i = i + 1
-	addi	$t0, $t0, 1	# duyet ki tu tiep thep
-	ble 	$t7, 75, x	# duyet den ki tu cuoi
-	
-	
-	addi 	$t8, $t8, 76	# duyet den xau tiep theo
-	ble 	$t8, $s1, loop2
-	
-	j MENU
-#---------------------------------------------------------------------------------------------------------------- 	
-yc3:				# dao chu
-li 	$v0, 4			# in ra message2
-	la	$a0, ms3
-	syscall
-	
 		
-# chinh sua	
-	move	$t8, $s0	# t8 chua dia chi xau dau tien
-loopx:
-	li	$t0, '\0'	# them ki tu ket thuc
-	sb 	$t0, 23($t8)
-	sb	$t0, 48($t8)
+	move	$t8, $s0					# t8 = s0 = first line
+	Case3_Loop1:
+		li	$t0, '\0'				# t0 = null
+		sb 	$t0, 23($t8)				# divide line into 3 strings
+		sb	$t0, 45($t8)
 	
-	li	$t1, ' '	# xoa ki tu  enter
-	sb	$t1, 73($t8)
+		li	$t1, ' '				# replace \n at end of line by  '  ' 
+		sb	$t1, 63($t8)
 	
-	addi 	$t8, $t8, 76	# duyet den xau tiep theo
-	ble 	$t8, $s1, loopx
+		addi 	$t8, $t8, 66				# traverse whole lines
+		ble 	$t8, $s1, Case3_Loop1
 	
-# in anh
-	move	$t8, $s0
+		move	$t8, $s0
+	Case3_Loop2:						
+		addi	$t8, $t8, 46 				# Print E
 	
-loop3:				
+		li 	$v0, 4
+		move 	$a0, $t8
+		syscall
+								
+		addi	$t8, $t8, -46				# Print C
+		addi	$t8, $t8, 24
 	
+		li 	$v0, 4
+		move 	$a0, $t8
+		syscall
+								
+		addi	$t8, $t8, -24				# Print D
 	
-# in E	
-	addi	$t8,$t8, 49		
+		li 	$v0, 4
+		move 	$a0, $t8
+		syscall
+							
+		li	$v0, 11					# Print \n
+		li	$a0, '\n'
+		syscall
 	
-	li 	$v0,4
-	move 	$a0, $t8
-	syscall
-	
-#in C
-	addi	$t8,$t8,-49
-	addi	$t8,$t8, 24
-	
-	li 	$v0,4
-	move 	$a0, $t8
-	syscall
-	
-#in D
-	addi	$t8,$t8,-24
-	
-	li 	$v0,4
-	move 	$a0, $t8
-	syscall
-	
-	
-#in enter
-	li	$v0,11
-	li	$a0, '\n'
-	syscall
-	
+		addi 	$t8, $t8, 66				# Traverse whole lines 
+		ble 	$t8, $s1, Case3_Loop2
 
-	addi 	$t8, $t8, 76	# duyet tat ca cac xau 
-	ble 	$t8, $s1, loop3
+		# reset Image
+		move	$t8, $s0				# t8 = s0 = first line
+	Case3_Loop3:
+		li	$t0, ' '				# replace \0 by  ' '
+		sb 	$t0, 23($t8)
+		sb	$t0, 45($t8)
+		
+		li	$t1, '\n'				# recover '\n	
+		sb	$t1, 63($t8)
+		
+		addi 	$t8, $t8, 66				# traverse whole lines
+		ble 	$t8, $s1, Case3_Loop3
+	j 	Menu
+###############################################################################
+Case4				:				# Change color
+	InputColorD:
+		li 	$v0, 4					# print \n
+		la 	$a0, ms1
+		syscall
+		
+		li 	$v0, 12					
+		syscall
+		move	 $s5, $v0				# s5 is new color of D
 	
-# reset
-
-	move	$t8, $s0	# t8 chua dia chi xau dau tien
-loopxx:
-	li	$t0, ' '	# xoa ki tu ket thuc
-	sb 	$t0, 23($t8)
-	sb	$t0, 48($t8)
+		blt	$s5, '0', InputColorD  			# check color (from 0 -> 9)
+		bgt	$s5, '9', InputColorD  
+	InputColorC:
+		li 	$v0, 4
+		la 	$a0, ms2
+		syscall
 	
-	li	$t1, '\n'	# khoi phuc ki tu  enter
-	sb	$t1, 73($t8)
+		li	$v0, 12					
+		syscall
+		move	$s6, $v0				# s6 is new color of C
 	
-	addi 	$t8, $t8, 76	# duyet den xau tiep theo
-	ble 	$t8, $s1, loopxx
-
+		blt	$s6, '0', InputColorC			# check color (from 0 -> 9)
+		bgt	$s6, '9', InputColorC
+	InputColorE:
+		li	$v0, 4
+		la	$a0, ms3
+		syscall
 	
-	j MENU
-#-------------------------------------------------------------------------------------------------------------------
-yc4:				# doi mau
-
-
-# nhap mau cho chu D
-nhapD:
-	li 	$v0, 4		#in message3
-	la 	$a0, ms7
-	syscall
+		li	$v0, 12					
+		syscall
+		move	$s7, $v0				# s7 is new color of E
 	
+		blt	$s7, '0', InputColorE			# check color (from 0->9)
+		bgt	$s7, '9', InputColorE
 	
-	li 	$v0, 12		# s5 chua ki tu mau cua chu D
-	syscall
-	move	 $s5, $v0
-	
-	blt	$s5, '0', nhapD	# kt ki tu mau 0->9
-	bgt	$s5, '9', nhapD
-	
-# nhap mau cho chu C	
-nhapC:
-
-	li 	$v0, 4
-	la 	$a0, ms8
-	syscall
-	
-	li	$v0, 12		# s6 chua ki tu mau cua chu C
-	syscall
-	move	$s6, $v0
-	
-	blt	$s6, '0', nhapC	# kt ki tu mau 0->9
-	bgt	$s6, '9', nhapC
-	
-# nhap mau cho chu E
-nhapE:
-	li	$v0, 4
-	la	$a0, ms9
-	syscall
-	
-	li	$v0, 12		# s7 chua ki tu mau cua chu E
-	syscall
-	move	$s7, $v0
-	
-	blt	$s7, '0', nhapE	# kt ki tu mau 0->9
-	bgt	$s7, '9', nhapE
-	
-# in message4
-	li 	$v0, 4		
-	la 	$a0, ms4
+	li 	$v0, 4					
+	la 	$a0, endofline
 	syscall	
 	
-# bat dau doi mau
+	move 	$t8, $s0 					# t8 = s0 = first line
+	Case4_Loop:						# Loop is change new color
+		move 	$t0, $t8				# t0 = t8 = adress of line
+		li	$t7, 0					# t7 is counter
+		Case4_Loop_2:					# t7 just count, and t3 is address of character
+			lb 	$t3, 0($t0)			# t3 save character in line
 
-	move 	$t8, $s0	# t8 chua dia chi xau dau tien
-	
-loop4:	
-	move 	$t0, $t8	
-	li	$t7, 0		# $t7 = i, i = 0
-	
-x4:	
-	lb 	$t3, 0($t0)	# t3 chua ki tu trong xau
+		CheckD:	
+			bgt	$t7, 23, CheckC 	 	# if t7 > 23 check C
+			beq	$t3, $s2, ChangeColorD 		# if t3= s2 ChangeColorD (s2 = current color of D)
+		CheckC:
+			bgt	$t7, 45, CheckE			# if t7 > 45 check E
+			beq	$t3, $s3, ChangeColorC		# if t3 = s3 ChangeColorC (s3 = current color of C)
+		CheckE:
+			beq	$t3, $s4, ChangeColorE		# s4 = current color of E
+			j 	Case4_Continue			# next character
 
-checkD:	
-	bgt	$t7, 23, checkC	# i > 23 thi kt C
-	beq	$t3, $s2, fixD
-	
-checkC:
-	bgt	$t7, 48, checkE	# i > 48 thi kt E
-	beq	$t3, $s3, fixC
-
-checkE:
-	beq	$t3, $s4, fixE
-	j 	next4		# duyet ki tu tiep theo
-	
-fixD:  		
-	sb 	$s5, 0($t0)
-	j next4
-	
-	
-fixC:
-	sb 	$s6, 0($t0)
-	j next4
-	
-fixE:	
-	sb 	$s7, 0($t0)
-	j next4
-	
-next4:	
-	addi 	$t7, $t7, 1	# i = i + 1
-	addi	$t0, $t0, 1	# duyet ki tu tiep thep
-	ble 	$t7, 75, x4	# duyet den ki tu cuoi
+		ChangeColorD:  		
+			sb 	$s5, 0($t0)			# t0 = s5 (= new color)
+			j 	Case4_Continue
+		ChangeColorC:
+			sb 	$s6, 0($t0)
+			j 	Case4_Continue
+		ChangeColorE:	
+			sb 	$s7, 0($t0)
+			j 	Case4_Continue
+		Case4_Continue:	
+			addi 	$t7, $t7, 1			# 
+			addi	$t0, $t0, 1			# 
+			ble 	$t7, 65, Case4_Loop_2		# traverse until and of line
 			
+		addi 	$t8, $t8, 66				# traverse whole lines
+		ble 	$t8, $s1, Case4_Loop	
 	
-	addi 	$t8, $t8, 76	# duyet den xau tiep theo
-	ble 	$t8, $s1, loop4
-	
-	jal 	print		# goi thu stuc print
-
-# update ki tu mau
-
+	jal 	ShowImage				
+	# update  new color of character
 	move	$s2, $s5
 	move	$s3, $s6
 	move	$s4, $s7
-		
-				
-	j MENU
-
-
+	j 	Menu
 EXIT:
-
-	
-	
-	
